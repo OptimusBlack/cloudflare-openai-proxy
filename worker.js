3e610a92c6d334acfbf95cc22b854b3251b56620
optimusblack <emilakhani@gmail.com>
2025-09-18 11:11:18 +0800

    initial commit

---
+++ worker.js
@@ -0,0 +1,33 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run "npm run dev" in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run "npm run deploy" to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+async function handleRequest(req, ctx) {
+  const url = new URL(req.url)
+  url.host = 'api.openai.com'
+  url.pathname = `/v1${url.pathname}`
+  
+  console.log(url.toString())
+  
+  return fetch(
+    url.toString(),
+    {
+      method: req.method,
+      headers: req.headers,
+      body: req.body,
+      signal: req.signal,
+    }
+  )
+}
+
+export default {
+  async fetch(request, env, ctx) {
+    return handleRequest(request, ctx);
+  }
+};
